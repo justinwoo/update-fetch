6158e7c7b2d6a93d4390b1b0d09d9d4c4c85bb1b
justinwoo <moomoowoo@gmail.com>
2019-11-26 12:36:52 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
+
+  binDeps = [ pkgs.nix-prefetch-git ];
+
+in pkgs.stdenv.mkDerivation rec {
+  name = "nixpkgs-fmt";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/justinwoo/update-fetch/releases/download/2019-11-26/update-fetch";
+    sha256 = "175j9z7abifkzfjnv5rxa2r96jg78xk4g897g34y6cdm0j03i08m";
+  };
+
+  buildInputs = [ pkgs.makeWrapper ];
+
+  dontStrip = true;
+
+  libPath = pkgs.lib.makeLibraryPath [ pkgs.glibc ];
+
+  unpackPhase = ''
+    mkdir -p $out/bin
+    TARGET=$out/bin/update-fetch
+
+    cp $src $TARGET
+    chmod +x $TARGET
+
+    patchelf $TARGET \
+      --interpreter ${dynamic-linker} \
+      --set-rpath ${libPath}
+
+    wrapProgram $TARGET \
+      --prefix PATH : ${pkgs.lib.makeBinPath binDeps}
+  '';
+
+  dontInstall = true;
+}
