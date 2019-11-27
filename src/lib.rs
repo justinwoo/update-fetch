146f97d4a239e3cb53f1d72cfd43bf25080c32dd
justinwoo <moomoowoo@gmail.com>
2019-11-27 20:46:26 +0200

    technically don't need to push to mutable vector

--- src/lib.rs
+++ src/lib.rs
@@ -16,22 +16,21 @@ pub struct Replacement {
 pub fn format(root: &SyntaxNode) -> FmtDiff {
     let mut model = FmtModel::new(root.clone());
 
-    let mut replacements: Vec<Replacement> = Vec::new();
-
-    for element in walk_non_whitespace(root) {
-        element
-            .as_node()
-            .and_then(|x| node::get_node_attr_set(&x))
-            .map(|node: &SyntaxNode| {
-                let set = node::attr_set_binds_to_hashmap(&node);
-                if let Some(method) = core::handle_fetch(&node, &set) {
-                    let range = node.text_range();
-                    let delete = TextRange::offset_len(range.start(), range.len());
-
-                    replacements.push(Replacement { delete, method });
-                }
-            });
-    }
+    let replacements: Vec<Replacement> = walk_non_whitespace(root)
+        .flat_map(|element| {
+            element
+                .as_node()
+                .and_then(|x| node::get_node_attr_set(&x))
+                .and_then(|node: &SyntaxNode| {
+                    let set = node::attr_set_binds_to_hashmap(&node);
+                    core::handle_fetch(&node, &set).map(|method| {
+                        let range = node.text_range();
+                        let delete = TextRange::offset_len(range.start(), range.len());
+                        Replacement { delete, method }
+                    })
+                })
+        })
+        .collect();
 
     let edits: Vec<AtomEdit> = replacements
         .par_iter()
